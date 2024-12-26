abd1de493c7e23a1511311db3862a1954b06d646
Joshesumare <iquiqueiquique2019@gmail.com>
2024-12-26 14:32:06 -0300

    Update page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,12 @@
 'use client';
-import { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import dynamic from 'next/dynamic';
 
-import { Abi, useBlockNumber, useAccount, useBalance, useReadContract, useNetwork, useContract } from "@starknet-react/core";
-import { ABI } from 'starknet';
+import { useAccount, useBalance, useBlockNumber, useContract, useReadContract, useSendTransaction, useTransactionReceipt, useCall } from "@starknet-react/core";
+import { BlockNumber, RpcProvider } from 'starknet';
+import { ABI } from '@/abis/abi';
+import { type Abi } from 'starknet';
+import { formatAmount } from '@/lib/utils';
 
 const WalletBar = dynamic(() => import('../components/WalletBar'), { ssr: false })
 const Page: FC = () => {
@@ -25,21 +28,44 @@ const { data: balanceData, error: balanceError, isError: balanceIsError, isLoadi
   // Step 2 --> Read your balance -- End
   // Step 3 --> Read counter from contract -- Start
   const contractAddress = "0x043df1763d11074b22bfedf3b71852d9947fd901666ddae41394411a7105d4c5";
-  const { data: readData, isError: readIsError, isLoading: readIsLoading, refetch: dataRefetch } = useReadContract({
+  const { data: readData, refetch: dataRefetch, isError: readIsError, isLoading: readIsLoading, error: readError } = useReadContract({
     functionName: "Consultar",
     args: [],
     abi: ABI as Abi,
     address: contractAddress,
     watch: true,
-    refetchInterval: 1000, 
+    refetchInterval: 1000 
   });
+
   // Step 3 --> Read counter from contract -- End
 
   // Step 4 --> Increase counter on contract -- Start
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await writeAsync();
+  } 
+  const typedABI =  ABI as Abi;
   const {contract} = useContract ({
-    abi,
-    address: testAddress,
-  });
+    abi: typedABI,
+    address: contractAddress,
+    });
+    const calls = useMemo(() => {
+      if (!userAddress || !contract) return [];
+      return [contract.populate("contadorIncreased")];
+    }, [userAddress, contract]);
+    const { send: writeAsync, data: writeData, isPending: writeIsPending } = useSendTransaction({
+      calls
+    });
+    const { data: waitData, status: waitStatus, isLoading: waitIsLoanding, isError: waitIsError, error: WaitError } = useTransactionReceipt ({
+      watch : true,
+      hash: writeData?.transaction_hash      
+    });
+
+
+  //function buttonContent(): React.ReactNode {
+   //throw new Error('Function not implemented.');
+  //}
+
   // Step 4 --> Increase counter on contract -- End
 
   // Step 5 --> Reset balance -- Start
@@ -49,19 +75,19 @@ const { data: balanceData, error: balanceError, isError: balanceIsError, isLoadi
   // Step 6 --> Get events from a contract -- End
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4 flex flex-col">
-      <h1 className="text-3xl font-bold text-center mb-6">Starknet Frontend Workshop in spanish</h1>
+    <div className="min-h-screen bg-black-100 p-4 flex flex-col">
+      <h1 className="text-3xl font-bold text-center mb-6" ><b> Starknet Frontend Workshop in spanish </b></h1>
 
       <div className="flex flex-wrap justify-center gap-4">
 
         <div className="w-full max-w-md space-y-4">
-          <div className="bg-white p-4 border-black border">
+          <div className="bg-black p-4 border-white border">
             <h2 className="text-xl font-bold mb-2">Wallet Connection</h2>
             <WalletBar />
           </div>
 
           { /* Step 1 -->  Read the latest block -- Start */ }
-          <div className={`p-4 border-black border ${blockNumberData! < workshopEnd ? "bg-green-500" : "bg-red-500"}`}>
+          <div className={`p-4 border-white ${blockNumberData! < workshopEnd ? "bg-green-500" : "bg-red-500"}`}>
             <h3 className="text-lg font-bold mb-2">Read the Blockchain</h3>
             <p>Current Block: {blockNumberData}</p>
             <p>{blockNumberData! < workshopEnd ? "Workshop is live" : "Workshop is over"}</p>
@@ -70,21 +96,21 @@ const { data: balanceData, error: balanceError, isError: balanceIsError, isLoadi
 
           {/* Step 2 --> Read your balance -- Start */}
           {
-          <div className="p-4 bg-white border-black border">
-            <h3 className="text-lg font-bold mb-2">Your Balance</h3>
-            <p>Symbol: {balanceData?.symbol }</p>
-            <p>Balance: {Number (balanceData?.formatted).toFixed(2)}</p>
+          <div className="p-4 bg-black border-white border">
+            <h3 className="text-lg font-bold mb-2">Tu Balance</h3>
+            <p>Symbol: {balanceData?.symbol}</p>
+            <p>Balance: {Number(balanceData?.formatted).toFixed(2)}</p>
           </div> }
           {/* Step 2 --> Read your balance -- End */}
 
           {/* Step 5 --> Reset balance by owner only -- Start */}
           { 
-          <div className="p-4 bg-white border-black border">
+          <div className="p-4 bg-black border-white border">
             <h3 className="text-lg font-bold mb-2">Reset Balance</h3>
             <button
               onClick={() => console.log("Resetting...")}
               disabled={false}
-              className="mt-2 border border-black text-black font-regular py-2 px-4 bg-yellow-300 hover:bg-yellow-500 disabled:bg-gray-300 disabled:cursor-not-allowed"
+              className="mt-2 border border-white text-white font-regular py-2 px-4 bg-yellow-300 hover:bg-yellow-500 disabled:bg-gray-300 disabled:cursor-not-allowed"
             >
               Reset Balance
             </button>
@@ -98,12 +124,12 @@ const { data: balanceData, error: balanceError, isError: balanceIsError, isLoadi
         <div className="w-full max-w-md space-y-4">
 
           {/* Step 3 --> Read from a contract -- Start */}
-          <div className="p-4 bg-white border-black border">
-            <h3 className="text-lg font-bold mb-2">contador balance</h3>
+          <div className="p-4 bg-black border-white border">
+            <h3 className="text-lg font-bold mb-2">Balance Contador</h3>
             <p>Balance: {readData?.toString()}</p>
             <button
               onClick={() => dataRefetch()}
-              className="mt-2 border border-black text-black font-regular py-1 px-3 bg-yellow-300 hover:bg-yellow-500"
+              className="mt-2 border border-white text-black font-regular py-1 px-3 bg-yellow-300 hover:bg-yellow-500"
             >
               Refresh
             </button>
@@ -112,22 +138,24 @@ const { data: balanceData, error: balanceError, isError: balanceIsError, isLoadi
 
           {/* Step 4 --> Write to a contract -- Start */}
           { 
-          <form className="bg-white p-4 border-black border">
+          <form onSubmit= {handleSubmit}  className="bg-black p-4 border-white border">
             <h3 className="text-lg font-bold mb-2">Increase Counter</h3>
             <button
               type="submit"
               className="mt-3 border border-black text-black font-regular py-2 px-4 bg-yellow-300 hover:bg-yellow-500 disabled:bg-gray-300 disabled:cursor-not-allowed"
+              disabled={!userAddress || writeIsPending }
             >
-              Send
+              {buttonContent()}
             </button>
+            {writeData?.transaction_hash && (
             <a
               href={`https://sepolia.voyager.online/tx/`}
               target="_blank"
               className="block mt-2 text-blue-500 hover:text-blue-700 underline"
-              rel="noreferrer"
-            >
+              rel="noreferrer">
               Check TX on Sepolia
             </a>
+          )}
           </form> }
           {/* Step 4 --> Write to a contract -- End */}
 
